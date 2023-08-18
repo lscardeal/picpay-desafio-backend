f452c76b26477065aef220c215ba581e0cc12470
Lucas Cardeal <lscardeal90@gmail.com>
2023-08-18 10:53:07 -0300

    Create transaction repository

---
+++ src/main/java/com/picpay/desafio/backend/repository/TransactionRepository.java
@@ -0,0 +1,16 @@
+package com.picpay.desafio.backend.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.picpay.desafio.backend.domain.entity.transaction.Transaction;
+
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+    
+    Optional<Transaction> findTransactionById(Long id);
+
+    // Optional<User> findUserBySender();
+
+    // Optional<User> findUserByReceiver();
+}
