62e839f01fe01fee147693f858b62a26a5e9ee95
Lucas Cardeal <lscardeal90@gmail.com>
2023-08-18 10:49:22 -0300

    Create src directory

---
+++ src/test/java/com/picpay/desafio/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.picpay.desafio.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
