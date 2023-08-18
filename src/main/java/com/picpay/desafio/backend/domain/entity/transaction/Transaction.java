3b6c36a5071d1119f5ea834007c323e3dfa013a5
Lucas Cardeal <lscardeal90@gmail.com>
2023-08-18 10:51:54 -0300

    Create transaction domain

---
+++ src/main/java/com/picpay/desafio/backend/domain/entity/transaction/Transaction.java
@@ -0,0 +1,40 @@
+package com.picpay.desafio.backend.domain.entity.transaction;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import com.picpay.desafio.backend.domain.entity.user.UserAccount;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Entity
+@Table(name = "TRANSACTIONS")
+@Data
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Transaction {
+    
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "sender_id")
+    private UserAccount sender;
+
+    @ManyToOne
+    @JoinColumn(name = "receiver_id")
+    private UserAccount receiver;
+
+    private BigDecimal value;
+
+    private LocalDateTime timestamp;
+}
