1e251e271aaf3e83e9ec96d3eadbf4d93bd2f399
Lucas Cardeal <lscardeal90@gmail.com>
2023-08-19 11:41:26 -0300

    Remove wrong adapter

--- src/main/java/com/picpay/desafio/backend/BackendApplication.java
+++ src/main/java/com/picpay/desafio/backend/BackendApplication.java
@@ -2,8 +2,10 @@ package com.picpay.desafio.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class BackendApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/picpay/desafio/backend/services/TransactionService.java
+++ src/main/java/com/picpay/desafio/backend/services/TransactionService.java
@@ -1,13 +1,13 @@
 package com.picpay.desafio.backend.services;
 
-import java.util.Optional;
+import java.time.LocalDateTime;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.picpay.desafio.backend.adapters.TransactionAdapter;
 import com.picpay.desafio.backend.domain.dtos.TransactionDTO;
 import com.picpay.desafio.backend.domain.entity.transaction.Transaction;
+import com.picpay.desafio.backend.domain.entity.user.User;
 import com.picpay.desafio.backend.repositories.TransactionRepository;
 import com.picpay.desafio.backend.services.validators.TransactionValidator;
 
@@ -21,19 +21,28 @@ public class TransactionService {
     private TransactionRepository transactionRepository;
 
     @Autowired
-    private TransactionAdapter transactionAdapter;
-
-    @Autowired
     private UserService userService;
 
     @Autowired
     private UserAccountService userAccountService;
 
-    public Transaction createTransaction(TransactionDTO transactionModel) {
-        Transaction transaction = this.transactionAdapter.fromDTOToEntity(transactionModel, userService);
+    public Transaction createTransaction(TransactionDTO transactionDTO) {
+        Transaction transaction = this.assembleTransaction(transactionDTO);
         this.transactionValidator.validate(transaction);
         transactionRepository.save(transaction);
         userAccountService.updateTransactionUserAccounts(transaction);
         return transaction;
+    }
+
+    private Transaction assembleTransaction(TransactionDTO transactionDTO) {
+        User sender = userService.getUserById(transactionDTO.senderId());
+        User receiver = userService.getUserById(transactionDTO.receiverId());;
+
+        return Transaction.builder()
+                        .senderAccount(sender.getUserAccount())
+                        .receiverAccount(receiver.getUserAccount())
+                        .value(transactionDTO.value())
+                        .timestamp(LocalDateTime.now())
+                        .build();
     }
 }
