9e661ba7a0f6b7385dbde885c38a1d456954ba43
Lucas Cardeal <lscardeal90@gmail.com>
2023-08-19 21:15:50 -0300

    Change Transaction.value column name due reserved keyword

--- src/main/java/com/picpay/desafio/backend/domain/entity/transaction/Transaction.java
+++ src/main/java/com/picpay/desafio/backend/domain/entity/transaction/Transaction.java
@@ -5,6 +5,7 @@ import java.time.LocalDateTime;
 
 import com.picpay.desafio.backend.domain.entity.user.UserAccount;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -29,15 +30,17 @@ public class Transaction {
     private Long id;
 
     @ManyToOne
-    @JoinColumn(name = "sender_id")
+    @JoinColumn(name = "sender_id", nullable = false)
     private UserAccount senderAccount;
 
     @ManyToOne
-    @JoinColumn(name = "receiver_id")
+    @JoinColumn(name = "receiver_id", nullable = false)
     private UserAccount receiverAccount;
 
+    @Column(name = "TRANSACTION_VALUE", nullable = false)
     private BigDecimal value;
 
+    @Column(nullable = false)
     private LocalDateTime timestamp;
 
     public Transaction(UserAccount senderAccount, UserAccount receiverAccount, BigDecimal value) {
